25f1aa766bfafbaad4318463a880b0fea4ea5ddb
Jose Jimenez <sharkyjj896@gmail.com>
2022-10-25 14:14:43 -0700

    This *might* fix the duplicate movement issue
    Or break everything because I need to add in planes

--- to_delete/color-changer.component.js
+++ to_delete/color-changer.component.js
@@ -1,8 +1,19 @@
 /* global AFRAME, NAF */
+//really wishing this was better commented 
+//so of course, everything has to be correctly be a registered component in a frame, color changer is the 
+//function (I think) name here. 
+
 AFRAME.registerComponent('color-changer', {
+  //then we have something called events, im guessing that lets
   events: {
+    //this fire when a click happens, which starts another function, goes to evt (im guessing 
+    //environmen? )
     'click': function (evt) {
+      //okay, top part is easy, changes material color
       this.el.setAttribute('material', { color: this.getRandomColor() });
+
+      //however, what the flying pig is this. 
+      //gives ownership to the clicker person maybe? hm. 
       NAF.utils.takeOwnership(this.el);
     }
   },
